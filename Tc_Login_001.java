fb11efd551ccfc6ec5a1d601771bb4bbdbf21557
vijaya204 <64839220+vijaya204@users.noreply.github.com>
2020-05-06 12:59:00 +0530

    Add files via upload

---
+++ Tc_Login_001.java
@@ -0,0 +1,24 @@
+package Login;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import Pom_Login.Tc_Login_1;
+
+public class Tc_Login_001 {
+	static { 
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\renuvijaya\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	}
+public static void main(String[] args) throws InterruptedException {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.snapdeal.com/");
+	Tc_Login_1 r2 = new Tc_Login_1(driver);
+	r2.signin();
+	r2.login();
+	driver.switchTo().frame(0);
+	r2.usernameOrEmailField();
+	r2.contin();
+	r2.password();
+	r2.login1();
+}
+}
